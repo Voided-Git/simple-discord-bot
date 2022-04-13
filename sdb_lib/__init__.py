cbd14ef1a2f27ea9080c8719eca7515c26ef52ef
Voided-Git <ryanjohnsjuan@outlook.com>
2022-04-13 10:38:49 +0100

    Added error and success embed builders

--- sdb_lib/__init__.py
+++ sdb_lib/__init__.py
@@ -1,6 +1,6 @@
 from .log import Log
 from .json_info import Config
-from .utils import info_embed
+from .utils import info_embed, success_embed, error_embed
 
 
 __version__ = 0.1
--- sdb_lib/utils.py
+++ sdb_lib/utils.py
@@ -1,5 +1,14 @@
 from discord import Embed, Color
+from .json_info import Config
 
 
 def info_embed(text: str):
     return Embed(color = Color.random(), description = text)
+
+
+def success_embed(text: str):
+    return Embed(color = Color.random(), description = f"{Config.tick} {text}")
+
+
+def error_embed(text: str):
+    return Embed(color = 0xa10000, description = f"{Config.cross} {text}")
