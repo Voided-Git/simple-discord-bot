f44bd60ae835e9d2a91d9c783d2b2ddbf1a39792
Voided-Git <ryanjohnsjuan@outlook.com>
2022-04-13 10:20:29 +0100

    Create errors.py

---
+++ exts/events/errors.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+
+
+class Errors(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        pass
+
+
+def setup(bot):
+    bot.add_cog(Errors(bot))
