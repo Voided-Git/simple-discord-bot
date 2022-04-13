a60e80de314e26aa361cc04907d6e263901c615e
Voided-Git <ryanjohnsjuan@outlook.com>
2022-04-13 17:31:30 +0100

    Added parse_time and is_int functions

--- sdb_lib/__init__.py
+++ sdb_lib/__init__.py
@@ -1,6 +1,6 @@
 from .log import Log
 from .json_info import Config, Messages
-from .utils import info_embed, success_embed, error_embed
+from .utils import info_embed, success_embed, error_embed, parse_time
 
 
 __version__ = 0.1
--- sdb_lib/utils.py
+++ sdb_lib/utils.py
@@ -12,3 +12,34 @@ def success_embed(text: str):
 
 def error_embed(text: str):
     return Embed(color = 0xa10000, description = f"{Config.cross} {text}")
+
+
+def is_int(value: str):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+
+def parse_time(time: str):
+    if is_int(time):
+        return int(time)
+    if not is_int(time[:-1]):
+        return None
+
+    _time = time[-1:].lower()
+    time = int(time[:-1])
+
+    if _time == "s":
+        return time
+    elif _time == "m":
+        return time * 60
+    elif _time == "h":
+        return time * 3600
+    elif _time == "d":
+        return time * 21600
+    elif _time == "w":
+        return time * 151200
+    elif _time == "y":
+        return time * 7884000
