335006a795d77681df238a84f894b415b7b0ba47
Voided-Git <ryanjohnsjuan@outlook.com>
2022-04-26 22:32:22 +0100

    Added load_json and is_int functions for ticketing

--- sdb_lib/__init__.py
+++ sdb_lib/__init__.py
@@ -1,6 +1,6 @@
 from .log import Log
-from .json_info import Config, Messages
-from .utils import info_embed, success_embed, error_embed, parse_time, list_exts
+from .json_info import Config, Messages, load_json
+from .utils import info_embed, success_embed, error_embed, parse_time, list_exts, is_int
 
 
 __version__ = 0.1
