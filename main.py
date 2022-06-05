5c06e1b5a9c6e95f3c7606b4d84d79a4cb861d32
Voided-Git <ryanjohnsjuan@outlook.com>
2022-06-05 10:07:29 +0100

    Added file check function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from discord import Activity, ActivityType, Game, Status, Streaming
+from discord import Activity, ActivityType, Status
 from discord.ext import commands
 from sdb_lib import Log, Config
 from os import listdir
+from json import dump
 
 
 bot = commands.Bot(
@@ -9,6 +10,34 @@ bot = commands.Bot(
     case_insensitive = True,
     help_command = None
 )
+
+
+def check_files():
+    try:
+        open("./config.json", "r").close()
+    except FileNotFoundError:
+        Log.error("No config file found, create a config file.")
+        exit()
+
+    try:
+        open("./data.json", "r").close()
+    except FileNotFoundError:
+        Log.warning("No data file found, creating data file.")
+        with open("./data.json", "w") as f:
+            dump(
+                {
+                    "logging": {
+                        "channel": 0
+                    },
+                    "tickets": {
+                        "number": 0,
+                        "category": 0,
+                        "support": 0,
+                        "tickets": []
+                    }
+                },
+                f, indent = 4
+            )
 
 
 def load_exts():
@@ -54,6 +83,9 @@ async def on_ready():
     Log.info(f"Registered {len(bot.application_commands)} slash commands")
     Log.info(f"{bot.user.name} ({bot.application_id}) online")
 
+
+Log.info("Checking files...")
+check_files()
 
 Log.info("Loading extensions...")
 errors = load_exts()
